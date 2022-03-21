adbc7b0df12f3e3755abe1521238e5ddb8418ea3
Andrea-Mattia <andrea.mattia94@gmail.com>
2022-03-21 16:58:55 +0100

    add: favicon, fake structure, some scss partials

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <div class="container site-header">
+      <!-- LOGO -->
+      <div class="site-logo">
+        <a href="javascript:history.go(0)">
+          <img src="../assets/img/boolgram-logo.png" alt="Boolgram logo" />
+        </a>
+      </div>
+      <!-- SEARCH -->
+      <div class="site-search">SITE SEARCH HERE</div>
+      <!-- USER -->
+      <div class="site-user">SITE USER HERE</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixins";
+
+header {
+  position: fixed;
+  width: 100%;
+  padding: 0.5rem;
+  background: #fff;
+  border-bottom: 1px solid #ccc;
+  .site-header {
+    @include df("vertical");
+    justify-content: space-between;
+    .site-logo {
+      width: 100px;
+    }
+  }
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <section class="container">
+      <div class="site-feed">
+        <div class="site-stories">STORIES COMPONENT HERE</div>
+        <div class="site-posts">POST COMPONENT HERE</div>
+      </div>
+      <div class="site-sidebar">SIDEBAR COMPONENT HERE</div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainContent",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixins";
+
+main {
+  section {
+    @include df("verical");
+    justify-content: space-between;
+    padding-top: 100px;
+    // fake content emulation
+    .site-feed {
+      flex-basis: 60%;
+      .site-stories {
+        height: 150px;
+        padding: 1.5rem;
+        margin-bottom: 2rem;
+        border: 1px solid #ccc;
+      }
+      .site-posts {
+        height: 500px;
+        padding: 1.5rem;
+        margin-bottom: 2rem;
+        border: 1px solid #ccc;
+      }
+    }
+  }
+}
+</style>
