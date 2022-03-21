d313c6e1f5d6f05ef63148c96518b902c734a77e
Andrea-Mattia <andrea.mattia94@gmail.com>
2022-03-21 20:35:34 +0100

    add: fake stories w/style

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="site-stories">STORIES GOES HERE</div>
+  <div class="site-stories">
+    <!-- placeholder content -->
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+    <div class="story">
+      <img src="../assets/img/user-avatar.png" alt="Profile avatar" />
+      <span>profile.name</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,10 +35,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/mixins";
+@import "../styles/vars";
+
 .site-stories {
-  height: 150px;
-  padding: 1.5rem;
+  @include df("");
+  justify-content: space-between;
+  padding: 2.5rem;
   margin-bottom: 2rem;
   border: 1px solid #ccc;
+  .story {
+    @include df("both");
+    flex-direction: column;
+    cursor: pointer;
+    img {
+      height: 65px;
+      border-radius: 50%;
+      background: linear-gradient(215deg, red, orange);
+      background-clip: padding-box;
+      padding: 3px;
+    }
+    span {
+      max-width: 65px;
+      margin-top: 0.5rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
 }
 </style>
