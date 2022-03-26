193875db00e888e497f52f252a04dc4dca942a1b
Andrea-Mattia <andrea.mattia94@gmail.com>
2022-03-26 15:52:07 +0100

    add: complete skeleton load layout, need animation

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -25,6 +25,75 @@
         <div class="post-cover"></div>
       </div>
     </div>
+
+    <aside class="site-sidebar">
+      <!-- SUGGESTED PROFILES -->
+      <div class="suggested">
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="suggested-profile">
+          <div class="profile-info">
+            <span class="profile-avatar"></span>
+            <div class="profile-name">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
   </section>
 </template>
 
@@ -120,6 +189,61 @@ section {
           rgba(238, 238, 238, 1) 50%,
           rgba(170, 170, 170, 1) 100%
         );
+      }
+    }
+  }
+  .site-sidebar {
+    position: absolute;
+    top: 280px;
+    right: 0;
+    width: calc(40% - 3rem);
+    padding: 0 0 1.5rem 1.5rem;
+    background: white;
+    .suggested {
+      @include df("horizontal");
+      flex-direction: column;
+      .suggested-profile {
+        margin: 1rem 0;
+        .profile-info {
+          @include df("vertical");
+          .profile-avatar {
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            background: linear-gradient(
+              -45deg,
+              rgba(170, 170, 170, 1) 0%,
+              rgba(238, 238, 238, 1) 50%,
+              rgba(170, 170, 170, 1) 100%
+            );
+          }
+          .profile-name {
+            @include df("");
+            flex-direction: column;
+            margin-left: 1.5rem;
+            span:first-child {
+              height: 10px;
+              width: 150px;
+              margin-bottom: 0.5rem;
+              background: linear-gradient(
+                90deg,
+                rgba(170, 170, 170, 1) 0%,
+                rgba(238, 238, 238, 1) 50%,
+                rgba(170, 170, 170, 1) 100%
+              );
+            }
+            span:last-child {
+              height: 10px;
+              width: 100px;
+              background: linear-gradient(
+                90deg,
+                rgba(170, 170, 170, 1) 0%,
+                rgba(238, 238, 238, 1) 50%,
+                rgba(170, 170, 170, 1) 100%
+              );
+            }
+          }
+        }
       }
     }
   }
